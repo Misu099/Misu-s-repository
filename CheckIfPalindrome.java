1ea33164ca665eda02e5157ac3a9ce6227937129
Misu099 <mihaialexvp@gmail.com>
2023-07-31 22:27:59 +0300

    un text ca sa sa mearga la pasul urmator

---
+++ CheckIfPalindrome.java
@@ -0,0 +1,20 @@
+
+public class CheckIfPalindrome {
+    String theReverseWord = "";
+
+    public boolean checkIfPalindrome(String theWord) {
+
+
+        for (int i = theWord.length() - 1; i >= 0; i--) {
+            theReverseWord = theReverseWord + theWord.charAt(i);
+        }
+
+        if (theWord.equals(theReverseWord)) {
+            return true;
+        } else
+            return false;
+
+
+    }
+}
+
