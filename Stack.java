6bbd819c8b1d8a9cc7b8cc78fe44fcb61c8ec8d3
Misu099 <mihaialexvp@gmail.com>
2023-07-31 22:20:37 +0300

    un text ca sa sa mearga la pasul urmator

--- Stack.java
+++ Stack.java
@@ -34,7 +34,6 @@ public class Stack {
         arr[++index] = num;
     }
 
-
     private void expandStack() {
         int[] tempArr = new int[arr.length + 5];
         for (int i = 0; i < arr.length; i++) {
