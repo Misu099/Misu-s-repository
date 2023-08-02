b15b4d34ca25a2d75042f3dd93724ebee7282e43
Misu099 <mihaialexvp@gmail.com>
2023-08-02 23:26:13 +0300

    Should work properly now.

--- IntegerToRomanNumeral.java
+++ IntegerToRomanNumeral.java
@@ -1,189 +1,27 @@
 public class IntegerToRomanNumeral {
-
-    private int num;
     private String result = "";
+    private int[] milestones={10000, 9000,5000,4000,1000,900,500,100,90,50,40,10,9,5,4,1};
+    private String[] milestonepair={"X̅","I̅X̅","V̅","MV̅","M","CM","D","C","XC","L", "XL","X", "IX", "V", "IV", "I" };
+ private int temp;
 
 
-    public String convert(int num) {
-        if (num < 1 || num > 9999) {
+    public String convert(int num){
+        if (num < 1 || num >10000){
             return null;
         }
-        while (num > 0) {
-            if (num > 999) {
-                num = thousands(num);
-            } else if (num > 99) {
-                num = hundreds(num);
-            } else if (num > 9) {
-                num = tens(num);
-            } else {
-                num = units(num);
+        result = "";
+        while (num > 0){
+            for (int i =0; i <milestones.length; i++){
+                temp = num - milestones[i];
+                if(temp >= 0){
+                  //  System.out.println("loop:" + i + " returns num:" + num + "and result:" + result);
+                    result = result + milestonepair[i];
+                    num = num - milestones[i];
+                    i--;
+                   // System.out.println("loop:" + i + " returns num:" + num + "and result:" + result);
+                }
             }
         }
         return result;
-    }
-
-
-    private int units(int num) {
-        switch (num) {
-            case (1):
-                result += "I";
-                return num - 1;
-            case (2):
-                result += "II";
-                return num - 2;
-            case (3):
-                result += "III";
-                return num - 3;
-
-            case (4):
-                result += "IV";
-                return num - 4;
-            case (5):
-                result += "V";
-                return num - 5;
-            case (6):
-                result += "VI";
-                return num - 6;
-            case (7):
-                result += "VII";
-                return num - 7;
-            case (8):
-                result += "VIII";
-                return num - 8;
-            case (9):
-                result += "IX";
-                return num - 9;
-            default:
-                return num;
-
-        }
-    }
-
-    private int tens(int num) {
-        int temp = num / 10;
-        switch (temp) {
-            case 1:
-                result += "X";
-                return num - (temp * 10);
-
-            case 2:
-                result += "XX";
-                return num - (temp * 10);
-
-            case 3:
-                result += "XXX";
-                return num - (temp * 10);
-
-            case 4:
-                result += "XL";
-                return num - (temp * 10);
-
-            case 5:
-                result += "L";
-                return num - (temp * 10);
-
-            case 6:
-                result += "LX";
-                return num - (temp * 10);
-
-            case 7:
-                result += "LXX";
-                return num - (temp * 10);
-
-            case 8:
-                result += "LXXX";
-                return num - (temp * 10);
-
-            case 9:
-                result += "XC";
-                return num - (temp * 10);
-            default:
-                return num;
-        }
-    }
-
-    private int hundreds(int num) {
-        int temp = num / 100;
-        switch (temp) {
-            case 1:
-                result += "C";
-                return num - (temp * 100);
-
-            case 2:
-                result += "CC";
-                return num - (temp * 100);
-
-            case 3:
-                result += "CCC";
-                return num - (temp * 100);
-
-            case 4:
-                result += "CD";
-                return num - (temp * 100);
-
-            case 5:
-                result += "D";
-                return num - (temp * 100);
-
-            case 6:
-                result += "DC";
-                return num - (temp * 100);
-
-            case 7:
-                result += "DCC";
-                return num - (temp * 100);
-
-            case 8:
-                result += "DCCC";
-                return num - (temp * 100);
-
-            case 9:
-                result += "CM";
-                return num - (temp * 100);
-            default:
-                return num;
-        }
-    }
-
-    private int thousands(int num) {
-        int temp = num / 1000;
-        switch (temp) {
-            case 1:
-                result += "M";
-                return num - (temp * 1000);
-
-            case 2:
-                result += "MM";
-                return num - (temp * 1000);
-
-            case 3:
-                result += "MMM";
-                return num - (temp * 1000);
-
-            case 4:
-                result += "MV̅";
-                return num - (temp * 1000);
-
-            case 5:
-                result += "V̅";
-                return num - (temp * 1000);
-            case 6:
-                result += "V̅M";
-                return num - (temp * 1000);
-
-            case 7:
-                result += "V̅MM";
-                return num - (temp * 1000);
-
-            case 8:
-                result += "V̅MMMM";
-                return num - (temp * 1000);
-
-            case 9:
-                result += "I̅X̅";
-                return num - (temp * 1000);
-            default:
-                return num;
-        }
     }
 }
--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+IntegerToRomanNumeral test= new IntegerToRomanNumeral();
 
-IntegerToRomanNumeral test =new IntegerToRomanNumeral();
+//for (int i =0; i<99; i++){
+//    System.out.println("loop:" + i + " returns:" + test.convert(i));
+//}
 
-        System.out.println(test.convert(4));
+        for (int i =10000; i>5000; i= i-435){
+    System.out.println("loop:" + i + " returns:" + test.convert(i));
+}
+
 
     }
 }
