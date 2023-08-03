ebcceac614e1d02421f6d181186e73539c5a5a38
Misu099 <mihaialexvp@gmail.com>
2023-08-03 09:42:33 +0300

    Should work properly now.

--- IntegerToRomanNumeral.java
+++ IntegerToRomanNumeral.java
@@ -1,8 +1,10 @@
 public class IntegerToRomanNumeral {
-    private int[] milestones = {10000, 9000, 5000, 4000, 1000, 900, 500, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    private String[] milestonepair = {"X̅", "I̅X̅", "V̅", "MV̅", "M", "CM", "D", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    private static final int[] MILESTONES = {10000, 9000, 5000, 4000, 1000,
+            900, 500, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String[] MILESTONESPAIRS = {"X̅", "I̅X̅", "V̅", "MV̅", "M",
+            "CM", "D", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
     private int temp;
-    String result = "";
+    private String result;
 
     public String convert(int num) {
         if (num < 1 || num > 10000) {
@@ -10,14 +12,12 @@ public class IntegerToRomanNumeral {
         }
         result = "";
         while (num > 0) {
-            for (int i = 0; i < milestones.length; i++) {
-                temp = num - milestones[i];
+            for (int i = 0; i < MILESTONES.length; i++) {
+                temp = num - MILESTONES[i];
                 if (temp >= 0) {
-                    //  System.out.println("loop:" + i + " returns num:" + num + "and result:" + result);
-                    result = result + milestonepair[i];
-                    num = num - milestones[i];
+                    result = result + MILESTONESPAIRS[i];
+                    num = num - MILESTONES[i];
                     i--;
-                    // System.out.println("loop:" + i + " returns num:" + num + "and result:" + result);
                 }
             }
         }
