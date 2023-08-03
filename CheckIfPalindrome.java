a79f1eff48038e495d3ed262afabeb755699b7d2
Misu099 <mihaialexvp@gmail.com>
2023-08-03 10:33:52 +0300

    Should work properly now.

--- CheckIfPalindrome.java
+++ CheckIfPalindrome.java
@@ -1,9 +1,19 @@
 
 public class CheckIfPalindrome {
-    public boolean ifPalindrome(String word) {
+    public boolean isPalindrome(String word) {
         word = word.toLowerCase();
         for (int i = 0; i < (word.length() / 2); i++) {
             if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isPalindrome (int x){
+        String y = Integer.toString(x);
+        for(int i = 0; (i< y.length()/2); i++){
+            if (y.charAt(i) != y.charAt(y.length()-1-i)){
                 return false;
             }
         }
