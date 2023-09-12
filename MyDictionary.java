fc50e04d89549c2896e905e25f658013028969be
Misu099 <mihaialexvp@gmail.com>
2023-09-12 23:09:43 +0300

    works

--- MyDictionary.java
+++ MyDictionary.java
@@ -1,29 +1,10 @@
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class MyDictionary {
 
-    private Map<String, String> myDictionary = new HashMap<>(); //TODO: make this final, you will not change it during the execution
+    private final Map<String, String> myDictionary = new HashMap<>();
 
-    public MyDictionary() {   //TODO: remove this, a no args constructor is made by the compiler if you do not provide any constructor
-    }
-
-    //TODO: make this as an use anywhere class, not dependent on console
-    public void addInput() {
-        Scanner sc = new Scanner(System.in);
-        String firstWord = "";
-        while (firstWord.isBlank()) {
-            System.out.println("First word:");
-            firstWord = sc.nextLine();
-        }
-        String secondWord = "";
-        while (secondWord.isBlank()) {
-            System.out.println("Second word:");
-            secondWord = sc.nextLine();
-        }
-        add(firstWord, secondWord);
-    }
 
     public void add(String firstWord, String secondWord) {
         myDictionary.put(firstWord.toLowerCase(), secondWord.toLowerCase());
